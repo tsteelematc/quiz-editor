a10279f451dc075053e34168516d70edead471a0
tsteelemadisoncollege <tsteele@madisoncollege.edu>
2021-03-22 12:41:37 -0500

    Messing with some attr and style binding...
    Not used very often, so who really cares : - )

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'quiz-editor';
+
+  titleColor = Date.now() % 2 == 0 ? 'red' : 'blue';
+
+  titleTooltip = this.titleColor;
+
+  titleTooltip2 = "Foo";
 }
