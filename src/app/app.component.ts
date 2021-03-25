3f36bb8c5f78ea6bab0c7fd32f4c025c379c39f8
Andritsch, Lisa <landritsch@madisoncollege.edu>
2021-03-25 15:09:52 -0500

    calling methods on quiz service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { QuizService } from './quiz.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
+  constructor(
+    private quizSvc: QuizService
+  ) {}
+
+  quizzes = [];
+
+  ngOnInit() {
+    this.quizzes = this.quizSvc.loadQuizzes();
+    console.log(this.quizzes);
+  }
+
   title = 'quiz-editor';
-
-  toolTip = new Date().getSeconds() % 2 == 0 ? 'blue' : 'red';
-
 }
