44b8cd2186e3a48c626dca876506e1e902d0f75a
Lisa Clemens <34919562+LelinStam@users.noreply.github.com>
2021-03-25 15:14:04 -0500

    quiz service working

--- src/app/quiz.service.ts
+++ src/app/quiz.service.ts
@@ -6,4 +6,26 @@ import { Injectable } from '@angular/core';
 export class QuizService {
 
   constructor() { }
+
+  loadQuizzes() {
+    const quizzesFromTheCloud = [
+      {
+        name: "Quiz Bear"
+        , questions: [
+          "Foo"
+          ,"Bar"
+        ]
+      }
+      ,{
+        name: "Quiz Cat"
+        , questions: [
+          "Foo"
+          ,"Bar"
+        ]
+      }
+    ];
+    return quizzesFromTheCloud;
+  }
+  
+
 }
