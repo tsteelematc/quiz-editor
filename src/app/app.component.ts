8494def2a78fca669a9d7cddd673665d00bd2aa8
Lisa Clemens <34919562+LelinStam@users.noreply.github.com>
2021-03-26 15:05:26 -0500

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component,
+OnInit } from '@angular/core';
+import { QuizService } from './quiz.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(    
+    private quizSvc: QuizService
+  ) {
+  }
+
+  quizzes =[];
+
+  ngOnInit(){
+    this.quizzes = this.quizSvc.loadQuizzes();
+    console.log(this.quizzes);
+  }
+
   title = 'quiz-editor';
+  //toolTip = new Date().getSeconds() % 2 == 0 ? 'blue' : 'red';
+
+  selectedQuiz = undefined;
+
+  selectQuiz(quiz) {
+    this.selectedQuiz = quiz;
+    
+  } //Can also do lambda, does the same thing
+  // selectQuiz = (quiz) =>{...}
 }
