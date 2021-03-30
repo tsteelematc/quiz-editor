ab32bf1de15410e41eda0b81f19352823c77a403
Lori Lee <44907206+lrlee1@users.noreply.github.com>
2021-03-30 11:50:18 -0500

    Generate Quiz Service

--- src/app/quiz.service.ts
+++ src/app/quiz.service.ts
@@ -6,4 +6,22 @@ import { Injectable } from '@angular/core';
 export class QuizService {
 
   constructor() { }
+
+  loadQuizzes() {
+      const quizzesFromTheCloud = [
+          {
+              name: "Quiz 1",
+              questions: [
+                  "foo",
+                  "bar"
+              ]
+          },
+          {
+              name: "Quiz 2",
+              questions: []
+          }
+      ];
+
+      return quizzesFromTheCloud;
+  }
 }
