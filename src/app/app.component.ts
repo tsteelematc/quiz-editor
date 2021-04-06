c3e28f17bb0bdaf59711cfb70154445505caaab7
tsteelemadisoncollege <tsteele@madisoncollege.edu>
2021-04-06 15:47:12 -0500

    We messed with promises...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -80,4 +80,65 @@ export class AppComponent implements OnInit {
       }
     ];    
   }
+
+  jsPromisesOne() {
+    
+    const n = this.quizSvc.getMagicNumber(true);
+    console.log(n); // ? ? ?
+
+    // Promises are also called "thenables"... For a reason...
+    n
+      .then(number => {
+        console.log(number); // ? ? ?
+
+        const n2 = this.quizSvc.getMagicNumber(true);
+        console.log(n2); // ? ? ?
+
+        //const await = 43;
+
+        n2
+          .then(number => console.log(number))
+          .catch(err => console.error(err))
+        ;
+      })
+      .catch(err => {
+        console.error(err)
+      })
+    ;
+  }
+
+  async jsPromisesTwo() {
+
+    //const await = 12;
+
+    try {
+      const n = await this.quizSvc.getMagicNumber(false);
+      console.log(n); // ? ? ?
+
+      const n2 = await this.quizSvc.getMagicNumber(true);
+      console.log(n2);
+    }
+
+    catch (err) {
+      console.error(err);
+    }
+  }
+
+  async jsPromisesThree() {
+
+    const n = this.quizSvc.getMagicNumber(true);
+    const n2 = this.quizSvc.getMagicNumber(true);
+
+    console.log(n, n2); // ? ? ?
+
+    try {
+      //const foo = await Promise.all([n, n2]);
+      const foo = await Promise.race([n, n2]);
+      console.log(foo);
+    }
+
+    catch (err) {
+      console.error(err);
+    }
+  }
 }
