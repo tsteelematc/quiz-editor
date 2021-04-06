4d47a5ce20837c3fa3ead6aae6555817583fb103
Lisa Clemens <34919562+LelinStam@users.noreply.github.com>
2021-04-06 14:58:57 -0500

    add display error

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,6 +17,7 @@ export class AppComponent implements OnInit {
   ) {}
 
   quizzes = [];
+  errorLoadingQuizzes = false;
 
   ngOnInit() {
     this.quizSvc
@@ -30,7 +31,10 @@ export class AppComponent implements OnInit {
         }
 
         // Lamda with the errors, if errors exist
-        , (err) => console.error(err)
+        , (err) => {
+           console.error(err)
+           this.errorLoadingQuizzes = true;
+        }
 
       )
     ;
