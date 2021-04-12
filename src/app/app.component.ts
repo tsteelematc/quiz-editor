4d8da7b215cf515d0fae32a799985695f01869f5
cvang00 <77701006+cvang00@users.noreply.github.com>
2021-04-11 21:21:45 -0500

    Calling a method on our quiz service...
    Next, do some Angular binding to show the quizzes...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { 
+  Component,
+  OnInit
+  } from '@angular/core';
+import { QuizService } from './quiz.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'quiz-editor';
+export class AppComponent implements OnInit {
 
-  toolTip = new Date().getSeconds() % 2 == 0 ? 'blue' : 'red';
+  constructor(
+    private quizSvc: QuizService
+  ) {}
+
+  quizzes = [];
+
+  ngOnInit() {
+    this.quizzes = this.quizSvc.loadQuizzes();
+    console.log(this.quizzes); 
+  }
+
+  title = 'quiz-editor';
 }
