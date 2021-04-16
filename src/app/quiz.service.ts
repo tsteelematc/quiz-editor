3fb3c436c2f19a810279b5f5c8f74455f46c5303
jcmann <jen.c.mann@gmail.com>
2021-04-15 22:48:15 -0500

    Added FormsModule and HttpClientModule

--- src/app/quiz.service.ts
+++ src/app/quiz.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'; 
 
 @Injectable({
   providedIn: 'root'
 })
 export class QuizService {
 
-  constructor() { }
+  constructor(builtInHttp: HttpClient) { }
 
   loadQuizzes() {
     const quizzes = [
